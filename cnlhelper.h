73b70f945c06a4bd8e7247070ada8f8556fb5b44
Lizhifun <admin@lizhifun.cn>
2021-01-30 18:46:50 +0800

    first commit

---
+++ cnlhelper.h
@@ -0,0 +1,25 @@
+﻿#ifndef CNLHELPER_H
+#define CNLHELPER_H
+
+#include <QObject>
+
+class CNLHelper : public QObject
+{
+    Q_OBJECT
+public:
+    explicit CNLHelper(QObject *parent = nullptr);
+    static CNLHelper *Instance();
+
+private:
+    static CNLHelper *curHelper;
+    QStringList listPY;
+    QStringList listJP;
+
+public:
+    QString CNToPY(const QString &cnstr);//汉语转拼音
+    QString CNToEL(const QString &cnstr);//汉语转所有字首字母
+    QString CNToFL(const QString &cnstr);//汉语转第一个汉字首字母
+
+};
+
+#endif // CNLHELPER_H
---
+++ mainwindow.cpp
@@ -0,0 +1,29 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QMessageBox>
+#include "cnlhelper.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_btn_Change_clicked()
+{
+    if(ui->tb_CN->toPlainText().trimmed().length() <= 0)
+    {
+        QMessageBox::warning(this,QStringLiteral("提示"),QStringLiteral("请输入内容!"));
+        return;
+    }
+    //转换并赋值
+    ui->tb_PY->setPlainText(CNLHelper::Instance()->CNToPY(ui->tb_CN->toPlainText()));
+    ui->tb_PY2->setPlainText(CNLHelper::Instance()->CNToEL(ui->tb_CN->toPlainText()));
+    ui->tb_PY3->setPlainText(CNLHelper::Instance()->CNToFL(ui->tb_CN->toPlainText()));
+}
